0fbfa6f96ba816591499d3fae07a8e8469e5a217
Dusty Shaw <shustydaw@gmail.com>
2022-02-14 22:30:51 -0700

    Played with variables

--- JavaScript/chapter1Java/ch1_alert.js
+++ JavaScript/chapter1Java/ch1_alert.js
@@ -1,4 +1,32 @@
 alert("Saying hi from a different file");
-let a = 10;
-// I set the value to ten
+let str1 = 'Laurence'; 
+let str2 = "Svekis"; 
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+
+console.log(str1, typeof str1);
+console.log(str2, typeof str2);
+console.log(val1, typeof val1);
+console.log(val2, typeof val2);
+console.log(myNum, typeof myNum);
+
+let a = "Hello";
+a = prompt("world");
 console.log(a);
+
+let m = 5;
+let b = 70;
+let c = "5";
+b++;
+console.log(b);
+
+const myArr2 = [];
+myArr2[10] = 'test'
+console.log(myArr2);
+console.log(myArr2[2]);
+
+const myArr3 = [3,6,8,9,3,55,553,434];
+myArr3.sort();
+myArr3.length = 0;
+console.log(myArr3[0]);
