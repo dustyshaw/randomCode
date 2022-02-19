8134943e4c376eb6acec5c6814f5cdcc973d5dbf
Dusty Shaw <shustydaw@gmail.com>
2022-02-19 14:46:33 -0700

    Final commit

--- JavaScript/chapter1Java/ch1_alert.js
+++ JavaScript/chapter1Java/ch1_alert.js
@@ -1,76 +1,9 @@
 
-// let currentPlayer = 1;
-// while (currentPlayer === 1);
-// {
-//     while (SticksLeft > 3) 
-//     {
-//         let sticksChosen = prompt(`There are ${SticksLeft} Sticks Left.  How many do you want to pick up, Player1?` );
-
-//         if (sticksChosen > 3 || sticksChosen < 1) 
-//         {
-//             alert(`You can only pick up 1 to 3 sticks per turn`);
-//         }
-//         else
-//         {
-//             SticksLeft = (SticksLeft - sticksChosen);
-//         }
-
-//         Player2Turn(SticksLeft);
-//         alert(`Player2 chose ${randomNumber} and now there ${Player2SticksLeft} sticks left!`);
-//         currentPlayer = 2;
-//     }
-// }
-// while (currentPlayer === 2)
-// {
-//     while (SticksLeft > 3) 
-//     {
-
-//         let sticksChosen = prompt(`There are ${SticksLeft} Sticks Left.  How many do you want to pick up, Player1?` );
-
-//         if (sticksChosen > 3 || sticksChosen < 1) 
-//         {
-//             alert(`You can only pick up 1 to 3 sticks per turn`);
-//         }
-//         else
-//         {
-//             SticksLeft = (SticksLeft - sticksChosen);
-//         }
-
-
-//         //alert(`Player2 chose ${randomNumber} and now there ${Player2SticksLeft} sticks left!`);
-//         currentPlayer = 1;
-//     }
-// }
-
-// function Player2Turn(SticksLeft)
-// {
-//     randomNumber = getRandomInt(4);
-//     if (getRandomInt < 1)
-//     {
-//         randomNumber = getRandomInt(4);
-//     }
-//     else
-//     {
-//         SticksLeft = (SticksLeft - randomNumber);
-//     }
-//     return Player2SticksLeft;
-// }
-
-// function getRandomInt(max) 
-// {
-//     return Math.floor(Math.random() * max);
-//   }
-
-
-
-
-
-
 //---------------------Sets sticks left at the beginning to 20.  sets a counter i.
 let SticksLeft = 20;
 let i = 0;
 
-//---------------------Loop that determines if SticksLeft is > 3 -----------
+//---------------------Loop that repeats game until someone wins -----------
 while (SticksLeft != 0) 
 {
     SticksLeft = Player1Turn(SticksLeft);
@@ -118,7 +51,6 @@ function Player2Turn(SticksLeft)
         if (player2SticksChosen < 1 || player2SticksChosen > 3) 
         {
             alert(`You can only chose 1 to 3 sticks.`);
-            //Player2Turn(SticksLeft);
             return SticksLeft;
         }
         return (SticksLeft - player2SticksChosen);
