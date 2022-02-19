7ac14ff4a0080affcea3ac62b8fa0de117dd0925
Dusty Shaw <shustydaw@gmail.com>
2022-02-19 14:32:18 -0700

    Working game

--- JavaScript/chapter1Java/ch1_alert.js
+++ JavaScript/chapter1Java/ch1_alert.js
@@ -60,41 +60,79 @@
 // {
 //     return Math.floor(Math.random() * max);
 //   }
-let turns = 0;
+
+
+
+
+
+
+//---------------------Sets sticks left at the beginning to 20.  sets a counter i.
 let SticksLeft = 20;
+let i = 0;
 
 //---------------------Loop that determines if SticksLeft is > 3 -----------
-
-SticksLeft = Player1Turn(SticksLeft);
-SticksLeft = Player2Turn(SticksLeft);
-
+while (SticksLeft != 0) 
+{
+    SticksLeft = Player1Turn(SticksLeft);
+    SticksLeft = Player2Turn(SticksLeft);
+}
 
 //---------------------Functions--------------------------------------------
 function Player1Turn(SticksLeft) 
 {
-    let SticksChosen = prompt(`player one, choose how many sticks.  There are ${SticksLeft} left`);
-    if (SticksChosen < 1 || SticksChosen > 3) {
-        alert(`You can only chose 1 to 3 sticks.`);
+    if (SticksLeft === 1)
+    {
+        return alert(`Player1 took the last stick! Player2 Won! Better luck next time. `);
     }
-    return (SticksLeft - SticksChosen);
+    let SticksChosen = prompt(`player one, choose how many sticks.  There are ${SticksLeft} left`);
+    if (SticksLeft > 3) 
+    {
+        if (SticksChosen < 1 || SticksChosen > 3) {
+            alert(`You can only chose 1 to 3 sticks.`);
+            return SticksLeft;
+        }
+        return (SticksLeft - SticksChosen);
+    }
+    if (SticksLeft <= 3)
+    {
+        if (SticksChosen > SticksLeft) {
+            alert(`You can only chose less sticks then there are sticks left.`);
+            return SticksLeft;
+        }
+        else
+        {
+            return (SticksLeft - SticksChosen);
+        }
+    }
 }
-
 
 function Player2Turn(SticksLeft) 
 {
-    let player2SticksChosen = prompt(`play3er two, choose how many sticks.  There are ${SticksLeft} left`);
-    if (player2SticksChosen < 1 || player2SticksChosen > 3) {
-        alert(`You can only chose 1 to 3 sticks.`);
+    if (SticksLeft == 1)
+    {
+        return alert(`Player2 took the last stick! Player1 Won!`);
     }
-    else {
+    let player2SticksChosen = prompt(`player two, choose how many sticks.  There are ${SticksLeft} left`);
+    if (SticksLeft > 3)
+    {
+        if (player2SticksChosen < 1 || player2SticksChosen > 3) 
+        {
+            alert(`You can only chose 1 to 3 sticks.`);
+            //Player2Turn(SticksLeft);
+            return SticksLeft;
+        }
         return (SticksLeft - player2SticksChosen);
-    } 
-}
-
-
-function CheckSticks(SticksLeft) 
-{
-    if (SticksLeft < 3) {
-        console.log("do something here ");
+    }
+    if (SticksLeft <= 3)
+    {
+        if (player2SticksChosen > SticksLeft) {
+            alert(`You can only chose less sticks then there are sticks left.`);
+            return SticksLeft;
+        }
+        else
+        {
+            return (SticksLeft - player2SticksChosen);
+        }
     }
 }
+
