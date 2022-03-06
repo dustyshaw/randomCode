1b6e80905c69105d618b69b42fd939be87893269
Dusty Shaw <shustydaw@gmail.com>
2022-03-06 14:45:31 -0700

    need to fix play log

--- JavaScript/SticksGame/gameUpdate.js
+++ JavaScript/SticksGame/gameUpdate.js
@@ -23,9 +23,9 @@ let button2 = document.getElementById("button2");
 let button3 = document.getElementById("button3");
 let currentPlayer = document.getElementById("currentPlayer");
 let sticksLeftDisplay = document.getElementById("SticksLeftDisplay");
-let playLog = document.getElementById("playLog");
 sticksLeftDisplay.innerHTML = 21;
 currentPlayer.innerHTML = 1;
+let playLog = document.getElementById("playLogList");
 
 console.log(sticksLeftDisplay);
 
@@ -34,16 +34,22 @@ button1.addEventListener("click", function () {
     let newPlayLogItem = document.createElement('li');
     sticksLeftDisplay.innerHTML -= 1;
     currentPlayer.innerHTML = 3 - currentPlayer.innerHTML;
-    newPlayLogItem.innerHTML = `<li>Player ${currentPlayer.innerHTML} chose${sticksLeftDisplay.innerHTML}</li>`;
+    newPlayLogItem.innerHTML = `<li>Player ${currentPlayer.innerHTML} chose 1 stick</li>`;
     playLog.appendChild(newPlayLogItem);
-    return newPlayLogItem;
+    
 });
 button2.addEventListener("click", function () {
     sticksLeftDisplay.innerHTML -= 2;
     currentPlayer.innerHTML = 3 - currentPlayer.innerHTML;
+    newPlayLogItem.innerHTML = `Player ${currentPlayer.innerHTML} chose 2 sticks`;
+    playLog.appendChild(newPlayLogItem);
+    
 });
 button3.addEventListener("click", function () {
     sticksLeftDisplay.innerHTML -= 3;
     currentPlayer.innerHTML = 3 - currentPlayer.innerHTML;
+    newPlayLogItem.innerHTML = `<li>Player ${currentPlayer.innerHTML} chose 2 sticks</li>`;
+    playLog.appendChild(newPlayLogItem);
+    
 });
 
