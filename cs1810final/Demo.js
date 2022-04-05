b3c6d0897c8d38a6cfde3c3110ad7de486eae763
Dusty Shaw <shustydaw@gmail.com>
2022-04-05 09:45:00 -0600

    added Demo.js

---
+++ cs1810final/Demo.js
@@ -0,0 +1,13 @@
+class Planet {
+    PlanetName;
+    PlanetVolume;
+}
+
+const Earth = new Planet();
+Earth.PlanetName = "Earth";
+Earth.PlanetVolume = 259875159532;
+
+const Jupiter = new Planet();
+Jupiter.PlanetName = "Jupiter";
+Jupiter.PlanetVolume = 343400000000000;
+
