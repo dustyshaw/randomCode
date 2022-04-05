0568e7b3b2128ddb9e0e6e1c0480c1f69029e98d
Dusty Shaw <shustydaw@gmail.com>
2022-04-05 10:51:44 -0600

    changed background.  Might need to change it back

--- cs1810final/Demo.js
+++ cs1810final/Demo.js
@@ -11,3 +11,19 @@ const Jupiter = new Planet();
 Jupiter.PlanetName = "Jupiter";
 Jupiter.PlanetVolume = 343400000000000;
 
+let CalculatedVolume = Jupiter.PlanetVolume / Earth.PlanetVolume;
+
+let Earths = document.getElementById('Earths');
+let Jupiter = document.getElementById('Jupiter');
+
+let selectPlanet = document.getElementById('selectPlanet');
+let selectContainingPlanet = document.getElementById('selectContainingPlanet');
+
+if (selectPlanet == Earths && selectContainingPlanet == Jupiter) {
+
+    let display = document.getElementById('display');
+    display.innerHTML = CalculatedVolume;
+    //display.appendChild(CalculatedVolume);
+    console.log(CalculatedVolume);
+
+}
