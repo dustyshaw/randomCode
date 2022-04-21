a912856150a19fdacc337ef2a4cd3e5ee945b4b6
Dusty Shaw <shustydaw@gmail.com>
2022-04-21 14:04:05 -0600

    added event listener for allposts

--- cs1810final/AllPosts.js
+++ cs1810final/AllPosts.js
@@ -1,9 +1,18 @@
-function SortData() {
-    var subjects =
-        document.querySelectorAll("[post]");
-    var subjectsArray = Array.from(subjects);
-    let sorted = subjectsArray.sort(comparator);
-    sorted.forEach(e =>
-        document.querySelector("#list").
-            appendChild(e));
-}
+// function SortData() {
+//     var subjects =
+//         document.querySelectorAll("[post]");
+//     var subjectsArray = Array.from(subjects);
+//     let sorted = subjectsArray.sort(comparator);
+//     sorted.forEach(e =>
+//         document.querySelector("#list").
+//             appendChild(e));
+// }
+window.addEventListener("load", function () {
+    let MarsTagButton = document.getElementById("MarsTagButton");
+    let MarsPosts = document.querySelectorAll('div[alt="MarsPost"]');
+    MarsTagButton.addEventListener("click", function () {
+        for (let MarsPost of MarsPosts) {
+            MarsPost.style.backgroundColor = "pink";
+        }
+    });
+});
