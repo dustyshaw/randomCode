d03012b150fbfe6ac17618acdaae17599046609b
Dusty Shaw <shustydaw@gmail.com>
2022-04-21 21:31:11 -0600

    Can't get stupid flex box to work!! >:(

--- cs1810final/AllPosts.js
+++ cs1810final/AllPosts.js
@@ -9,10 +9,25 @@
 // }
 window.addEventListener("load", function () {
     let MarsTagButton = document.getElementById("MarsTagButton");
-    let MarsPosts = document.querySelectorAll('div[alt="MarsPost"]');
+    let AstronautsTagButton = document.getElementById("AstronautsTagButton");
+    
+    let AstronautPosts = document.querySelectorAll('section[alt="Astronauts"]');
+    let MarsPosts = document.querySelectorAll('section[alt="MarsPost"]');
+
+
     MarsTagButton.addEventListener("click", function () {
         for (let MarsPost of MarsPosts) {
             MarsPost.style.backgroundColor = "pink";
+            MarsPost.style.borderColor = "black";
         }
     });
+    AstronautsTagButton.addEventListener("click", function () {
+        for (let AstronautPost of AstronautPosts) {
+            AstronautPost.style.backgroundColor = "pink";
+        }
+    });
+
 });
+// window.addEventListener("load", function () {
+
+// });
