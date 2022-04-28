e68ddadb0774f7241303e6817c437bd974269afc
Dusty Shaw <shustydaw@gmail.com>
2022-04-28 15:04:50 -0600

    rearanged files

--- cs1810final/AllPostsTab/AllPosts.js
+++ cs1810final/AllPostsTab/AllPosts.js
@@ -1,12 +1,4 @@
-// function SortData() {
-//     var subjects =
-//         document.querySelectorAll("[post]");
-//     var subjectsArray = Array.from(subjects);
-//     let sorted = subjectsArray.sort(comparator);
-//     sorted.forEach(e =>
-//         document.querySelector("#list").
-//             appendChild(e));
-// }
+
 window.addEventListener("load", function () {
     let MarsTagButton = document.getElementById("MarsTagButton");
     let AstronautsTagButton = document.getElementById("AstronautsTagButton");
