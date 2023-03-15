2630813ba40c69fb723068c924c82d53c8986e18
Dusty Shaw <94818206+dustyshaw@users.noreply.github.com>
2023-03-14 20:19:37 -0600

    Delete JavaScript directory

--- JavaScript/Exam1Practice/program.js
+++
@@ -1,9 +0,0 @@
-let table = document.getElementById('output');
-let button = document.querySelectorAll(button);
-
-button.addEventListener("click", function(){
-    console.log("creating a new row?");
-    let newTableRow = document.createElement(tr);
-    newTableRow.innerHTML=`<td>this is a new table cell</td>`;
-    table.appendChild(newTableRow);
-});
